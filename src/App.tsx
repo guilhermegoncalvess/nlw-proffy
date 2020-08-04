6122760c7f534937ca6406b2adfe453cf8f02f8b
Guilherme Gonçalves <guilhermegufes@gmail.com>
2020-08-04 01:01:44 -0300

    aula1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Routes from './routes';
+
+import './assets/styles/global.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+    <Routes />
   );
 }
 
